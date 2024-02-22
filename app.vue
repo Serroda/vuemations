7dfbbf5d519e3d12e3c331a027ef78211e2f632a
serroda <jesuscarclem30@gmail.com>
2024-02-22 16:42:18 +0100

    Config for deploy

--- app.vue
+++ app.vue
@@ -4,7 +4,18 @@
   </NuxtLayout>
 </template>
 <script setup lang="ts">
+useHead({
+  title: "Vuemations",
+})
+useSeoMeta({
+  title: 'Vuemations',
+  ogTitle: 'Vuemations',
+  description: 'Animation collection for Vue 3',
+  ogDescription: 'Animation collection for Vue 3',
+})
+
 const animationsStyle = await useFetch("/api/files/all")
+
 onMounted(() => {
   const s = document.createElement("style")
   s.appendChild(document.createTextNode(animationsStyle.data.value as string))
